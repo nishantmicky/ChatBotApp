eb5a676ee19a19f4310992ab137ff84a6f5c6c34
Nishant Kumar <nishantkumar@Nishants-MacBook-Pro.local>
2025-05-01 11:50:07 +0530

    Initial commit

---
+++ ChatBotApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatBotApp
+//
+//  Created by Nishant Kumar on 01/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
